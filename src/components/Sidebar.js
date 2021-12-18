045e82b675b07d10be2ac7414bdae2cbeec2ef42
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-18 00:29:06 -0500

    Add font awesome icon, imported sublinks and useGlobalContext

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
+import sublinks from '../data'
+import { useGlobalContext } from '../context'
+
+
 
 const Sidebar = () => {
     return (
         <div>
-            
+           
         </div>
     )
 }
