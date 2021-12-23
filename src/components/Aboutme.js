4c500b8778d1293e3505f418adff071b9a080ef5
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-22 22:45:53 -0500

    Add a div with classname of my_cardInfo that will contain the details of the mycard

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import CardImage from '../images/backgroundImage_ccexpress.png'
+
 
 const Aboutme = () => {
     return (
         <div className="aboutcontainer">
           <div className="mycard">
+            <img src={CardImage} alt="Logo" />
+            <div className="my_cardInfo">
 
+            </div>
           </div>
         </div>
     )
