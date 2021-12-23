cf16271d2cafc41abf9910bed87b1bbddab84a6a
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-22 22:47:40 -0500

    Add a span for my name

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -4,11 +4,11 @@ import CardImage from '../images/backgroundImage_ccexpress.png'
 
 const Aboutme = () => {
     return (
-        <div className="aboutcontainer">
-          <div className="mycard">
+        <div className="about_container">
+          <div className="my_card">
             <img src={CardImage} alt="Logo" />
             <div className="my_cardInfo">
-
+               <span className="my_name"></span>
             </div>
           </div>
         </div>
