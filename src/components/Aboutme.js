aec6133a1fc24b2d2df00b43d57f0386fbe063ff
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-23 02:36:51 -0500

    style aboutme page

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -4,14 +4,22 @@ import CardImage from '../images/backgroundImage_ccexpress.png'
 
 const Aboutme = () => {
     return (
-        <div className="about_container">
+        <div className="container">
           <div className="my_card">
             <img src={CardImage} alt="Logo" />
             <div className="my_cardInfo">
+                <br />
                 <span className="tag">Sexiest Man Alive</span>
+                <br></br>
                <span className="my_name">Daniel Amoateng</span>
-               <div className="my_fullname"></div>
-               <p>lorem ipsum dolor sit amet, consectetur</p>
+                <br />
+               <p>It is a long established fact that a 
+                reader will be distracted by the readable content of a page when looking at its layout. 
+                The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                 as opposed to using 'Content here, content here', making it look like readable English. 
+                 Many desktop publishing packages and web page editors now.
+        
+</p>
                <button>Download Resume</button>
             </div>
             
