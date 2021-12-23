d518b04c0e03829d60b71333a78c64c3548c4330
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-23 13:43:37 -0500

    create one row two columns and add media query

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -7,6 +7,9 @@ const Aboutme = () => {
       
 
       <div className="card">
+          <div className="grid grid--1x2">
+              <img src={CardImage} alt="Daniel" />
+          </div>
 
       </div>
     
