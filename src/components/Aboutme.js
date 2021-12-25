e8713def0150929225f7c696b3c3dabab94c6500
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-24 23:06:01 -0500

    edit about me

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -9,18 +9,17 @@ const Aboutme = () => {
           <div className="grid grid--1x2">
               <img className="card-image" src={CardImage} alt="Daniel" />
               <div className="paragraph">
-                  <h3>About</h3>
+                  <h2>About Me</h2>
+                  <br />
                   <Readmore>
-                       There are many variations of passages of Lorem Ipsum available,
-                       but the majority have suffered alteration in some form, by injected humour, or 
-                       randomised words which don't look even slightly believable. Add more text here for more
-                       the mean time.
-                       
-                        There are many variations of passages of Lorem Ipsum available,
-                       but the majority have suffered alteration in some form, by injected humour, or 
-                       randomised words which don't look even slightly believable. Add more text here for more
-                       the mean time.
-             </Readmore>
+                       Hi there, I'm Daniel, a Software Engineer based in Auburn, Massachusetts. I have lived in Auburn for 5 years now, 
+                       and oh I love it here. It's awesome here!
+                       <br />
+                       Quick:
+                       So I don't have a TV and my friends think it's wierd. I kind of disagree. They think I don't do anything
+                       fun. Well, it will come as a shock when they find out that I enjoy problem solving, and I watch sports. That is it.  I think those are fun, at least to me.
+                       So, yeah. But let me know if you want to connect and do some problem solving with me. Cheers!
+                 </Readmore>
                   
               </div>
           </div>
