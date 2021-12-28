2441caf9c2573c31cbc2228d4504d06811685dc2
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-28 13:03:02 -0500

    Add social media styling

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,5 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLinkedIn,
-  faInstagram,
+  faGitHub,
   } from "@fortawesome/free-brands-svg-icons";
+
+
+  import React from 'react'
+  
+  const SocialMedia = () => {
+      return (
+          <div className="social-container">
+            <a href="https://www.twitter.com/jamesqquick" className="LinkedIn social">
+            <FontAwesomeIcon icon={faLinkedIn} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/learnbuildteach"
+            className="Github social">
+            <FontAwesomeIcon icon={faGitHub} size="2x" />
+            </a>
+          </div>
+      )
+  }
+  
+  export default SocialMedia
+  
