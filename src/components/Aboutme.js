bba8088808b7480cf3803f75d864bbe244aecf4b
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-28 13:35:32 -0500

    add social icons

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -11,12 +11,9 @@ const Aboutme = () => {
               <article className="social-media">
               <img className="card-image" src={CardImage} alt="Daniel" />
               <div className="socialMedia">
-
               <SocialMedia />
               </div>
               </article>
-
-             
               <div className="paragraph">
                    
                   <h4 className="aboutMe">About Me</h4>
@@ -27,15 +24,12 @@ const Aboutme = () => {
                        </p>
                        <p>
                       <p className="para">
-                       <h4>Fun fact:</h4>
+                       <h4>Quick Story:</h4>
                        </p>
                        <p className="para">
-                       So one day in July of 2016, I needed a haircut but I really wasn't interested in going to the barbershop on that day. So I thought, hmm, If I dont want to go to the barbershop, maybe I can look up some barbers in my area who might be interested in offering home service appointments for extra charge. Checked the AppStore, nothing. So I created that app so I could get a haircut at home lol. It had some thousand plus downloads though.
+                       So one day in July of 2016, I needed a haircut but I really wasn't interested in going to the barbershop on that day. So I thought, hmm, If I dont want to go to the barbershop, maybe I can look up some barbers in my area who might be interested in offering home service appointments for extra charge. Checked the AppStore, nothing. So I created that app so I could get a haircut at home.
                        </p>
-                       <p className="para">
-                       Let's connect!
-                       Cheers
-                       </p>
+                       
 
                        </p>
                  
