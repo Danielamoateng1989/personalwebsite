82dfc4fc24fffc3d896dea83fd72b47fcaa6637f
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-28 13:04:26 -0500

    Add social media links to socialmedia component

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -10,10 +10,10 @@ import {
   const SocialMedia = () => {
       return (
           <div className="social-container">
-            <a href="https://www.twitter.com/jamesqquick" className="LinkedIn social">
+            <a href="https://www.linkedin.com/in/daniel-amoateng-959a27181" className="LinkedIn social">
             <FontAwesomeIcon icon={faLinkedIn} size="2x" />
             </a>
-            <a href="https://www.instagram.com/learnbuildteach"
+            <a href="https://github.com/Danielamoateng1989"
             className="Github social">
             <FontAwesomeIcon icon={faGitHub} size="2x" />
             </a>
