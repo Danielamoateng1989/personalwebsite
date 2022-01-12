b754ac52b2a1841ec4ff8130301a69cab4201c40
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-01-12 03:30:48 -0500

    create new form 1. Add new form 2. Add input and text area 3. Add state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,73 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Form = () => {
+    const [name, SetName] = useState('')
+    const [email, SetEmail] = useState('')
+    const [message, SetMessage] = useState('')
+    const [isLoading, SetIsLoading] = useState('')
+
+
+   const navigate = useNavigate()
+   
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+
+        SetIsLoading(true)
+
+        fetch("http://localhost:8000/blogs", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: []
+        }).then(() => {
+            console.log('Blog saved')
+            SetIsLoading(false)
+            //After the user is done submitting the form, let's have them go back
+            navigate('/about')
+        })
+    }
+
     return (
-        <div>
-            
+        <div className="send-message">
+            <h1>Send me a message</h1>
+            <form onSubmit={handleSubmit}>
+
+                <label>name</label>
+                <input
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => SetName(e.target.value)}
+                    placeholder="name"
+                />
+
+
+                <label>Email</label>
+                <input
+                    required
+                    value={email}
+                    onChange={(e) => SetEmail(e.target.value)}
+                    placeholder="email"
+                />
+
+
+
+                <label>Message</label>
+                <textarea
+                    value={message}
+                    onChange={(e) => SetMessage(e.target.value)}
+                    placeholder="message"
+                
+                  
+                />
+
+                {!isLoading && <button className="button">Send</button>}
+                {/* {!isPending && <button disabled >Adding blog...</button>} */}
+            </form>
+
         </div>
     )
 }
