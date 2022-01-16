d4e63a9ecb3c9cd3e06232d9effac5a5d179c69c
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-01-15 20:44:04 -0500

    chore()

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -9,25 +9,24 @@ const Thankyou = () => {
 
 
     return (
-        
-            <div>
-                <div className="content">
-                    <div className="wrapper-1">
-                        <div className="wrapper-2">
-                            <h2>Thank you!</h2>
-                            <p>Thanks for connecting with me </p>
-                            <p>I will get back to you as soon as possible</p>
-                        <ul>
-                        <li>
-                            <button className='go-home' onClick={goHome}>
-                               go home
-                            </button>
-                        </li>
-                        </ul>
-                        </div>
-                    </div>
+        <React.Fragment>
+            <div className="wrapper-1">
+                <div className="wrapper-2">
+                    <h2>Thank you!</h2>
+                    <p>Thanks for connecting with me </p>
+                    <p>I will get back to you as soon as possible</p>
+                <ul>
+                <li>
+                    <button className='go-home' onClick={goHome}>
+                        go home
+                    </button>
+                </li>
+                </ul>
                 </div>
-        </div>
+            </div>
+        </React.Fragment>
+                
+       
     )
 }
 
