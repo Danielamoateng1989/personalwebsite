5c1a7d5f97984fa5b4b4170ede59bcba64e8fe83
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-01-23 14:36:09 -0500

    Add to logo to go to homepage

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,55 @@
 import React from 'react'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 
 // const axios = require('axios');
 
 
 const Form = () => {
-    const [name, SetName] = useState('')
-    const [email, SetEmail] = useState('')
-    const [message, SetMessage] = useState('')
-    const navigate = useNavigate();
-    const thankyou = () => navigate('/thankyou');
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    
+    // const navigate = useNavigate();
+    // const thankyou = () => navigate('/thankyou');
 
     
     const handleSubmit = (e) => {
         e.preventDefault()
+  
     
     }
     return (
         <div className="">
-        <div className="send-message">
+        <div className="send-message" onClick={handleSubmit}>
             <h3>contact</h3>
-            <form onSubmit={handleSubmit} className="section-form">
+            <form className="section-form">
                 <label>name</label>
                 <input
                     type="text"
                     required
                     value={name}
-                    onChange={(e) => SetName(e.target.value)}    
+                    onChange={e => setName(e.target.value)} 
+                    
                 />
                 <label>Email</label>
                 <input
                     required
                     value={email}
-                    onChange={(e) => SetEmail(e.target.value)}   
+                    onChange={(e) => setEmail(e.target.value)}   
                     
                 />
                 <label>Message</label>
                 <textarea
                     value={message}
-                    onChange={(e) => SetMessage(e.target.value)}    
-                />
-                <button className="profile-card__button button--blue" type="submit" onClick={thankyou}>Send</button>
-            </form>
+                    onChange={(e) => setMessage(e.target.value)}    
+                />            
+                <button className="profile-card__button button--blue" type="submit">Send</button>
 
-            
+                </form>
+
+
         </div>
         </div>
     )
